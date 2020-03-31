44a2dad7e82f4553614c824d76f256fff477f49b
dan-5691 <62926400+dan-5691@users.noreply.github.com>
2020-03-31 09:50:42 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/
+// @match        https://WMWejI_zO2S1zGe0n0zidaqdjAoj8GK09m8nThI1NW00000uyjiTG1H80c2g672kiFZ9dSemc07uoPtACBW1hh0Cs07m_UaLs082y0BgYxl01yJc0VW4j-rFY0MtxK-G1RVjJyMioYqL7QWHqGR5G02b5Hse4VsdYg2nmwiU5gjKokc9bjmVWF08bvZKm3lABR_0wPX9hwUA003gIkUZ3gBe2xVjJ_0B1kWC6-0DWe35sQhkqgA7r16W3i24FTdW3m604FZ-zDWIWO0Oy182yl_Il92niO0Mu1EtxK-W5BVjJwWKj-rFw1G8s1N1YlRieu-y_6Fma1PXe1RGwUdp1SaM00000000y3yT0BfixghWXtfZXjCDXz1BzKEimpQiXmQ1eJAGykha5esTUVLGF6AJQWt4IdIdKmYD8OrFO3m2xa91X-ELDuiC16CM7SD2r_Isg0ReRnE1Oy4kbbcfVYq8C0E8DN4nCPu0~1/*
+// ==/UserScript==
+
+let yandexInput = document.getElementsByName('text')[0];
+yandexInput.value = "Гобой";
+
 
